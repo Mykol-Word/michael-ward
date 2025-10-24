44b62824346deea500239bd03ee827671804f8de
Mykol Word <michaelward1916@gmail.com>
2025-10-24 12:55:49 -0700

    setup github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mykol-word.github.io',
+  base: '/michael-ward',
   devToolbar: {
     enabled: false
   },
